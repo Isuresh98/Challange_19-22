95d26cc7b2d193a1ee0e5b39a06297914839afa4
Indunil Suresh <102247770+Isuresh98@users.noreply.github.com>
2023-01-31 23:59:59 +0530

    player  movement

---
+++ Assets/Script/Player_Movement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    [SerializeField] float speed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+    Vector3 movement = new Vector2(horizontalInput, 0);
+    transform.position += movement* speed * Time.deltaTime;
+        transform.position += movement* speed * Time.deltaTime;
+    }
+}
