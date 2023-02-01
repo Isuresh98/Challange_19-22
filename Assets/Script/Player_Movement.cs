8df66362eab891e95a30819e5c81160834d5e491
Indunil Suresh <102247770+Isuresh98@users.noreply.github.com>
2023-02-01 12:24:36 +0530

    19 complate

--- Assets/Script/Player_Movement.cs
+++ Assets/Script/Player_Movement.cs
@@ -5,23 +5,55 @@ using UnityEngine;
 public class Player_Movement : MonoBehaviour
 {
     [SerializeField] float speed = 10f;
+    [SerializeField] float _jumpForce = 10f;
+    private Rigidbody2D rbody;
+    public bool isGrounded;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         Movement();
+        Jump();
+         
+       
     }
 
     private void Movement()
-    {
+    { 
         float horizontalInput = Input.GetAxis("Horizontal");
-    Vector3 movement = new Vector2(horizontalInput, 0);
-    transform.position += movement* speed * Time.deltaTime;
+           Vector3 movement = new Vector2(horizontalInput, 0);
+          transform.position += movement* speed * Time.deltaTime;
         transform.position += movement* speed * Time.deltaTime;
+
+    }
+
+    void Jump()
+    {
+       
+        if (isGrounded && Input.GetButtonDown("Jump"))
+        {
+            rbody.velocity = Vector2.up * _jumpForce * Time.deltaTime;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+        
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
     }
 }
