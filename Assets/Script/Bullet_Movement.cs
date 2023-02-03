68bff39f1169627a934e78df8685799c4cc41e90
Indunil Suresh <102247770+Isuresh98@users.noreply.github.com>
2023-02-03 15:21:39 +0530

    recod create

--- Assets/Script/Bullet_Movement.cs
+++ Assets/Script/Bullet_Movement.cs
@@ -5,27 +5,26 @@ using UnityEngine;
 public class Bullet_Movement : MonoBehaviour
 {
     [SerializeField] float _bulatSpeed;
-    Rigidbody2D _rbody;
+    Rigidbody2D _rBody;
     // Start is called before the first frame update
     void Start()
     {
-        _rbody = GetComponent<Rigidbody2D>();
+        _rBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        _rbody.AddForce(transform.right * _bulatSpeed* Time.deltaTime, ForceMode2D.Impulse);
-        Destroy(gameObject, 1f);
+        _rBody.AddForce(transform.right * _bulatSpeed * Time.deltaTime);
+        Destroy(gameObject,2f);
     }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Enemy"))
         {
-            
             Destroy(collision.gameObject);
             Destroy(gameObject);
-          
         }
     }
 }
--- Assets/Script/Player_Movement.cs
+++ Assets/Script/Player_Movement.cs
@@ -4,87 +4,78 @@ using UnityEngine;
 
 public class Player_Movement : MonoBehaviour
 {
-    public bool isGrounded;
-    private Rigidbody2D rbody;
-    [SerializeField] SpriteRenderer _srender; 
-
-    [SerializeField] float speed = 10f;
-    [SerializeField] float _jumpForce = 10f;
-    [SerializeField]private int ScoreCount;
+    [SerializeField] float _speed = 10f;
+    [SerializeField] float _jumForce = 10f;
+    public int _ScoreCount;
+    Rigidbody2D _rbody;
+    bool isGround;
+    private SpriteRenderer _spriteRender;
     [SerializeField] private GameObject _bulatPrefabs;
     // Start is called before the first frame update
     void Start()
     {
-        rbody = GetComponent<Rigidbody2D>();
-        _srender = GetComponent<SpriteRenderer>();
+        _rbody = GetComponent<Rigidbody2D>();
+        _spriteRender = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
         Movement();
         Jump();
         Shoot();
-    }
 
+    }
+  
     private void Movement()
-    { 
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
-           Vector3 movement = new Vector2(horizontalInput, 0);
-          transform.position += movement* speed * Time.deltaTime;
-        transform.position += movement* speed * Time.deltaTime;
-
+        Vector3 movemrnt = new Vector2(horizontalInput, 0);
+        transform.position += movemrnt * _speed * Time.deltaTime;
     }
-
     void Jump()
     {
-       
-        if (isGrounded && Input.GetButtonDown("Jump"))
+        if (isGround&&Input.GetButtonDown("Jump"))
         {
-            rbody.velocity = Vector2.up * _jumpForce * Time.deltaTime;
-        }
-    }//Jump
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            isGrounded = true;
-        }
-        if (collision.gameObject.CompareTag("DounGround"))
-        {
-            isGrounded = true;
-            Rigidbody2D DounGroundRB = collision.gameObject.GetComponent<Rigidbody2D>();
-            if (DounGroundRB != null)
-            {
-                DounGroundRB.bodyType = RigidbodyType2D.Dynamic;
-            }
-        }
+            _rbody.velocity = Vector2.up * _jumForce * Time.deltaTime;
 
-        if (collision.gameObject.CompareTag("Enemy"))
-        {
-            ScoreCount += 5;
-            Destroy(collision.gameObject);
-            _srender.color = Color.grey;
         }
-
-    }//OnCollisionEnter2D
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            isGrounded = false;
-        }
-        
-
-
-    }//OnCollisionExit2D
-
+    }
     void Shoot()
     {
         if (Input.GetMouseButtonDown(0))
         {
             Instantiate(_bulatPrefabs, transform.position, Quaternion.identity);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGround = true;
+        }
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            _ScoreCount += 5;
+            Destroy(collision.gameObject);
+            _spriteRender.color = Color.gray;
+        }
+        if (collision.gameObject.CompareTag("DounGround"))
+        {
+            isGround = true;
+            Rigidbody2D DounGround_rBody = collision.gameObject.GetComponent<Rigidbody2D>();
+
+            if(DounGround_rBody != null)
+            {
+                DounGround_rBody.bodyType = RigidbodyType2D.Dynamic;
+            }
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGround = false;
         }
     }
 }
